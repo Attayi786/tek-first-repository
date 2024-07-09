f86ff569208c9e7f95d9a29c129659f8b3cb7907
mydocuments <hamed.mr.unique@gmail.com>
2024-07-08 23:35:20 -0700

    my second commit hmd

---
+++ src/hello/world/NewClass.java
@@ -0,0 +1,11 @@
+package hello.world;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hamed");
+        System.out.println("A");
+    }
+
+
+}
